f213dede908f5958201e5ee512567e6cd063d5d6
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-03-07 12:14:35 +0200

    Created React App

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+import Main from "./components/Main"
+
+function App() {
+  return (
+    <Main /> // using the Main component
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export default function Main() {
+    // state is data that makes every component update when it changes
+    const [state, setState] = useState("abcd");
+
+    // setState is the function that changes the state
+    const changeState = () => {
+        if(state === "abcd") setState("efgh");
+        else setState("abcd");
+    }
+
+    // useEffect is used for functions that need to run
+    // every time some state changes or when the component first renders
+    useEffect(() => {
+        console.log("state changed");
+    }, [state]); // dependency array (this executes when state changes)
+
+    return (<main className="main-component">
+        <p>Hello World!</p>
+        <p>{ state }</p>
+        <button onClick={() => changeState()}>Change State</button>
+    </main>);
+
+}
