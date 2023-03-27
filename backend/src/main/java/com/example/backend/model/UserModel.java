b4a139d359f05b3ebc1603f637295da3ab829808
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-03-27 18:45:01 +0300

    Implemented user details

--- backend/src/main/java/com/example/backend/model/UserModel.java
+++ backend/src/main/java/com/example/backend/model/UserModel.java
@@ -5,19 +5,57 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collection;
+import java.util.List;
+
+import javax.management.relation.Role;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 @Entity
 @Getter @Setter 
 @NoArgsConstructor @AllArgsConstructor
-public class UserModel {
+@Table(name = "users")
+public class UserModel implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String username;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // returns a list of the user's roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getRoleName()));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;        
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
