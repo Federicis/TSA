d4431a9c099201ad3aab1368e98b52fffd9dbee2
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-03-27 18:49:56 +0300

    Created user repository

---
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.UserModel;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUsername(String username);
+    
+}
