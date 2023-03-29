dffce1fafbc759d49aa10ec308bb83b633ed46a7
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-03-29 19:16:37 +0300

    Updated to Spring 3

--- backend/src/main/java/com/example/backend/model/ExampleModel.java
+++ backend/src/main/java/com/example/backend/model/ExampleModel.java
@@ -4,10 +4,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @Data
