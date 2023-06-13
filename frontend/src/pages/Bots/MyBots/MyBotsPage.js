38248ca45401768e9c7f8c5b3aeedd9ecb48ca14
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-06-13 15:47:00 +0300

    Created basic carousel

--- frontend/src/pages/Bots/MyBots/MyBotsPage.js
+++ frontend/src/pages/Bots/MyBots/MyBotsPage.js
@@ -4,10 +4,14 @@ import "./MyBots.css";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import AuthContext from "../../../context/AuthProvider";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 import { useNavigate } from "react-router-dom";
 
 export default function MyBotsPage() {
 	const [bots, setBots] = useState([]);
+	const [currIndex, setCurrIndex] = useState(0);
 
 	const { auth } = useContext(AuthContext);
 	const axiosPrivate = useAxiosPrivate();
@@ -26,4 +30,37 @@ export default function MyBotsPage() {
 		};
 		getBots();
 	}, []);
+
+	return (
+		<main className="my-bots">
+			<h1 className="my-bots-heading">My Bots</h1>
+			{bots.length ? (
+				<div className="bots-carousel">
+					<FontAwesomeIcon
+						onClick={() => setCurrIndex((currIndex + bots.length - 1) % bots.length)}
+						className="prev-bot-button"
+						icon={faAngleLeft}
+					/>
+					{[1, 2, 3, 4, 5].map((num) => (
+						<div className={`bot-card bot-card-${num}`} key={num}>
+							{num == 3 ? (
+								<h2 className="bot-name">
+									{bots[(currIndex + num - 1) % bots.length].name}
+								</h2>
+							) : (
+								<></>
+							)}
+						</div>
+					))}
+					<FontAwesomeIcon
+						onClick={() => setCurrIndex((currIndex + 1) % bots.length)}
+						className="next-bot-button"
+						icon={faAngleRight}
+					/>
+				</div>
+			) : (
+				<p>You have no bots.</p>
+			)}
+		</main>
+	);
 }
