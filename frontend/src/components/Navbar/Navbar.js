51f7cbe6fd6e0de9c487986363c19aeda546cf6b
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-06-13 16:14:35 +0300

    made navbar responsive

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,16 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faHome,
+	faSignOutAlt,
+	faPlus,
+	faRobot,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Navbar() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	const isMobile = windowWidth < 750;
+
 	return (
 		<nav className="navbar">
 			<div className="links">
-				<Link to="/">Home</Link>
-				<Link to="my-bots">My Bots</Link>
-				<Link to="create-bot">Create Bot</Link>
+				<Link to="/">{isMobile ? <FontAwesomeIcon icon={faHome} /> : "Home"}</Link>
+				<Link to="my-bots">
+					{isMobile ? <FontAwesomeIcon icon={faRobot} /> : "My Bots"}
+				</Link>
+				<Link to="create-bot">
+					{isMobile ? <FontAwesomeIcon icon={faPlus} /> : "Create Bot"}
+				</Link>
 			</div>
-			<Link to="logout">Logout</Link>
+			<Link to="logout">
+				{isMobile ? <FontAwesomeIcon icon={faSignOutAlt} /> : "Log Out"}
+			</Link>
 		</nav>
 	);
 }
