61b7a60a4ea4f79aff6a666594d2a0a5a7774018
Victor1128 <victorbadulescu7@gmail.com>
2023-06-16 14:24:39 +0300

    completed edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import RequireAuth from "./components/RequireAuth";
 import { AuthProvider } from "./context/AuthProvider";
 import CreateBot from "./pages/Bots/CreateBot/CreateBot";
 import MyBotsPage from "./pages/Bots/MyBots/MyBotsPage";
+import EditBotPage from "./pages/Bots/EditBot/EditBotPage";
 import LoginPage from "./pages/Login/Login";
 import SignUpPage from "./pages/SignUp/SignUp";
 import Navbar from "./components/Navbar/Navbar";
@@ -13,24 +14,25 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<AuthProvider>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<PersistLogin />}>
-						<Route element={<RequireAuth />}>
-							<Route index element={<Main prop1={"abcd"} prop2={"efgh"} />} />
-							<Route path="create-bot" element={<CreateBot />} />
-							<Route path="my-bots" element={<MyBotsPage />} />
-						</Route>
-						<Route path="login" element={<LoginPage />} />
-						<Route path="sign-up" element={<SignUpPage />} />
-					</Route>
-				</Routes>
-			</AuthProvider>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PersistLogin />}>
+            <Route element={<RequireAuth />}>
+              <Route index element={<Main prop1={"abcd"} prop2={"efgh"} />} />
+              <Route path="create-bot" element={<CreateBot />} />
+              <Route path="my-bots" element={<MyBotsPage />} />
+              <Route path="edit-bot" element={<EditBotPage />} />
+            </Route>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="sign-up" element={<SignUpPage />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
