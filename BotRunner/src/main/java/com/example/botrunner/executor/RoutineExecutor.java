27164e517398356c29aaa1d121cd08e7222bcdd2
alin090402 <alin090402@yahoo.ro>
2023-06-18 22:04:21 +0300

    Bot runner with spring (#24)
    * BotRunner - Initialized project + database sync + run tasks (right now it supports a primitive version of find and comment)
    
    * Update BotRunnerApplication.java
    
    * updated squeduler and records
    
    ---------
    
    Co-authored-by: alin090402 <alin.sava02@gmail.com>
    Co-authored-by: Federicis <59761771+Federicis@users.noreply.github.com>

--- BotRunner/src/main/java/com/example/botrunner/executor/RoutineExecutor.java
+++ BotRunner/src/main/java/com/example/botrunner/executor/RoutineExecutor.java
@@ -2,38 +2,57 @@ package com.example.botrunner.executor;
 
 import ca.arnah.reddit4j.RedditUserClient;
 import com.example.botrunner.database.models.RoutineModel;
+import com.example.botrunner.database.models.record.RoutineRecord;
 import com.example.botrunner.database.models.task.TaskModel;
+import com.example.botrunner.database.services.RecordService;
 import com.example.botrunner.executor.taskExecutor.TaskExecutor;
 import com.example.botrunner.executor.taskExecutor.TaskExecutorFactory;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RoutineExecutor {
+@Getter
+public class RoutineExecutor implements Runnable {
     private final RunningData runningData;
     private final RoutineModel routine;
     private final RedditUserClient redditUserClient;
     private final List<TaskExecutor> taskExecutors = new ArrayList<>();
+    private final RoutineRecord record = new RoutineRecord();
 
 
     public RoutineExecutor(RoutineModel routine, RedditUserClient redditUserClient) {
         this.routine = routine;
         this.runningData = new RunningData();
         this.redditUserClient = redditUserClient;
+        record.setRoutine(routine);
     }
     public RoutineExecutor(RunningData runningData, RoutineModel routine, RedditUserClient redditUserClient) {
         this.runningData = runningData;
         this.routine = routine;
         this.redditUserClient = redditUserClient;
+        record.setRoutine(routine);
     }
 
-    public void execute() {
+    public void run() {
         prepare();
-        for (TaskExecutor taskExecutor : taskExecutors) {
-            taskExecutor.execute(runningData, redditUserClient);
+        try{
+            for (TaskExecutor taskExecutor : taskExecutors) {
+                taskExecutor.execute(runningData, redditUserClient);
+            }
+            record.setWasSuccessful(true);
+            record.setMessage("Routine executed successfully");
+        }
+        catch (Exception e)
+        {
+            record.setWasSuccessful(true);
+            record.setMessage(e.getMessage());
         }
     }
-
     private void prepare(){
         for(TaskModel task : routine.getTasks()){
             taskExecutors.add(TaskExecutorFactory.createTaskExecutor(task));
